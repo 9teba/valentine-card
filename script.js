03e1c1b70d967ced683ec0047ae03247076a8488
9teba <tebaalazo80@gmail.com>
2025-01-30 15:54:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+body {
+    font-family: 'Arial', sans-serif;
+    text-align: center;
+    background-color: #ffe6e6;
+    color: #d6336c;
+    padding: 50px;
+}
+
+.container {
+    background: white;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+    display: inline-block;
+}
+
+h1 {
+    color: #ff4081;
+}
